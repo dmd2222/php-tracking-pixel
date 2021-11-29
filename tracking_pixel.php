5a9c149fe4f5ed806cdded6c1448a4ddfc60b031
dmd2222 <dmd20@gmx.de>
2021-11-29 04:20:50 +0100

    Update tracking_pixel.php

--- tracking_pixel.php
+++ tracking_pixel.php
@@ -9,7 +9,7 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-/*Version: 1.0.2.0 First Edition */
+/*Version: 1.0.2.1 First Edition */
 
 
 //options
